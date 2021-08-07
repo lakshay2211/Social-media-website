33d983806838fcf5abe6fe3c915c3191440de9f8
lakshay2211 <42599567+lakshay2211@users.noreply.github.com>
2021-08-07 19:05:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,16 +11,10 @@ const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
 
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
-
-// setup the chat server to be used with socket.io
-const chatServer = require('http').Server(app);
-const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000);
-console.log('chat server is listening on port 5000');
 
 
 app.use(sassMiddleware({
@@ -60,13 +54,11 @@ app.use(session({
     cookie: {
         maxAge: (1000 * 60 * 100)
     },
-    store: new MongoStore(
+    store: MongoStore.create(
         {
-            mongooseConnection: db,
-            autoRemove: 'disabled'
-        
+            mongoUrl: 'mongodb://localhost/codeial-development',
         },
-        function(err){
+        function(err){ 
             console.log(err ||  'connect-mongodb setup ok');
         }
     )
